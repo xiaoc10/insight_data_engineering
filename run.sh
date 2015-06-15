30a36d3815ac2ae965f0d73a81d5c1e761f35a8d
xiaoc10 <xiaoc10@163.com>
2015-06-15 18:47:53 -0500

    test in ubuntu

--- run.sh
+++ run.sh
@@ -1,4 +1,2 @@
-#chmod a+x solution.py
-
-# finally I'll execute my programs, with the input directory wc_input and output the files in the directory wc_output
+chmod a+x ./src/solution.py
 python ./src/solution.py ./wc_input ./wc_output/wc_result.txt ./wc_output/med_result.txt
