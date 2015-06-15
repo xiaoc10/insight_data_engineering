f3fdaf25c4881aefb10df099d7a5f8ba1318fb09
xiaoc10 <xiaoc10@163.com>
2015-06-15 16:30:33 -0500

    add comment

--- src/solution.py
+++ src/solution.py
@@ -1,5 +1,25 @@
 __author__ = 'Zhong Zhang'
 
+'''
+(1) Word Count
+Idea:
+use a map to store the word as the key and its associated occurrences as the value.
+
+(2) Stream Median
+Idea:
+The key of the problem is how to insert the coming number into the existed array efficiently.
+One of the optimal solutions is to use balanced binary search tree, insertion is O(logN) and
+finding median is O(1). However, the implementation of balanced binary search tree, for example
+AVL, is pretty complexity. We can achieve the same time complexity by maintaining two heaps
+simultaneously, a max-heap and min-heap.
+
+The max-heap is used to store the smallest half of the numbers and min-heap contains the largest
+half which means the maximum element in the max-heap is less than the minimum element in the
+min-heap. If the number of elements is even, the max-heap and min-heap contain half of the
+elements separately. If the number is odd, the max-heap has one more element than the min-heap.
+
+Zhong Zhang @ UTA @ 6/15/2015
+'''
 import fnmatch
 import os
 import re
@@ -14,19 +34,19 @@ class stream_median:
         self.count = 0
 
     def insert(self, num):
-        if self.count % 2 == 0:
+        if self.count % 2 == 0: #self.count % 2 == 0 ==> even ==> insert new element to max_heap
             heapq.heappush(self.max_heap, -1*num)
             self.count = self.count + 1
             if len(self.min_heap) > 0:
-                if -1*self.max_heap[0] > self.min_heap[0]:
-                    max_left = -1*heapq.heappop(self.max_heap)
+                if -1*self.max_heap[0] > self.min_heap[0]:     # if maximum element in max_heap >= minimum element in min_heap
+                    max_left = -1*heapq.heappop(self.max_heap) # adjust max_heap and min_heap
                     min_right = heapq.heappop(self.min_heap)
                     heapq.heappush(self.max_heap, -1*min_right)
                     heapq.heappush(self.min_heap, max_left)
-        else:
-            max_left = -1*heapq.heappushpop(self.max_heap, -1*num)
-            heapq.heappush(self.min_heap, max_left)
-            self.count = self.count + 1
+        else: #self.count % 2 != 0 ==> odd ==> insert new element to min_heap
+            max_left = -1*heapq.heappushpop(self.max_heap, -1*num) #compare the current element with the maximum element
+            heapq.heappush(self.min_heap, max_left)                #in the max_heap and the larger one is inserted to
+            self.count = self.count + 1                            #min_heap
 
     def get_median(self):
         if self.count % 2 == 0:
@@ -71,19 +91,14 @@ class insight_data_engineering_solver:
         return txtfiles
 
     def word_count(self):
-        #src_fullfiles = [os.path.join(self.src_dir, txtfile) for txtfile in self.txtfiles ]
         counter = word_counter(self.fullfiles)
         word_and_counts = counter.count_word()
         sorted_word_and_counts = sorted(word_and_counts.items(), key=operator.itemgetter(0))
 
-        #output word counting results
-        #destfile = os.path.join(self.src_dest, "wc_result.txt")
         destfile = self.dest_file_q1
-        with open(destfile, "w") as file_stream:
+        with open(destfile, "w") as file_stream: #output word counting results
             for item in sorted_word_and_counts:
                 file_stream.write("{0}\t{1}\n".format(item[0], item[1]))
-                #file_stream.write("{0}\t{1}" % item[0], item[1])
-        #print(sorted_x)
 
     def running_median(self):
         median_calculator = stream_median()
@@ -95,16 +110,12 @@ class insight_data_engineering_solver:
                     for line in file_stream: #per line
                         line1 = re.sub(r'[^a-zA-Z0-9\s]+', "", line).lower().strip()
                         word_size = len(line1.split()) #eliminate empty line
-                        #print(word_size)
                         median_calculator.insert(word_size)
                         median = median_calculator.get_median()
                         output_stream.write("{0}\n".format(int(median*10)/10.0))
-                        #print(format())
-                        #print("%.*f" % (median_calculator.get_median(), 1))
+
 
 def solver(src_dir, dest_file_q1, dest_file_q2):
-    #solver = insight_data_engineering_solver("..\wc_input", "..\wc_output")
-    #print(src_dir, dest_file_q1, dest_file_q2)
     ide_solver = insight_data_engineering_solver(src_dir, dest_file_q1, dest_file_q2)
     ide_solver.word_count()
     ide_solver.running_median()
@@ -112,24 +123,3 @@ def solver(src_dir, dest_file_q1, dest_file_q2):
 if __name__ == "__main__":
     params=sys.argv[1:]
     solver(*params)
-
-#solver("..\wc_input", "..\wc_output\wc_result.txt", "..\wc_output\med_result.txt")
-
-'''
-counter = word_counter("..\wc_input")
-word_and_counts = counter.count_word()
-sorted_x = sorted(word_and_counts.items(), key=operator.itemgetter(0))
-print(sorted_x)
-'''
-'''
-def word_count(src_files):
-def print_result():
-#txt_files = []
-for file in os.listdir('../wc_input'):
-    if fnmatch.fnmatch(file, '*.txt'):
-        #word_count(file, dest_file)
-        #txt_files.extend([file])
-        txt_files = txt_files + [file]
-word_count(txt_files)
-print(txt_files)
-'''
