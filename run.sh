7ef945cfd64b1e66cb62025db359502dd21fa7d9
xiaoc10 <xiaoc10@163.com>
2015-06-15 17:38:09 -0500

    add shell script run.sh

---
+++ run.sh
@@ -0,0 +1,4 @@
+#chmod a+x solution.py
+
+# finally I'll execute my programs, with the input directory wc_input and output the files in the directory wc_output
+python ./src/solution.py ./wc_input ./wc_output/wc_result.txt ./wc_output/med_result.txt
